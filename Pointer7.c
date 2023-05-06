91ff20ffde13855312362d4b1a3a674d266a7143
Tejas More <97433987+TejasMore05@users.noreply.github.com>
2023-05-06 20:31:15 +0530

    Add files via upload

---
+++ Pointer7.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int Addition(int No1, int No2)          // 1000 from text section
+{
+    int Ans = 0;
+
+    Ans = No1 + No2;
+
+    return Ans;
+}
+
+int main()
+{
+    int Value1 = 10, Value2 = 11;
+    int Ret = 0;
+
+    int (*ptr)(int , int);
+
+    ptr = Addition;
+
+    // Ret = Addition(Value1,Value2);
+
+    Ret = ptr(Value1,Value2);
+    
+    printf("%d\n",Ret); // 21
+
+    return 0;
+}
+
